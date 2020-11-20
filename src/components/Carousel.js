20223dfb935098669414da447c2539d3f4ce7f94
ChristyTropila <tropila04@gmail.com>
2020-11-20 15:54:11 -0500

    updated

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import spotpotpic from '../assets/images/spotpotpic.PNG'
-import sweaterweatherpic from '../assets/images/sweaterweatherpic.PNG'
+import image1 from '../assets/images/image1.png'
+import image3 from '../assets/images/image3.png'
 import profilepic from '../assets/images/profilepic.jpg'
+import image8 from '../assets/images/image8.png'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Card from '../components/Card'
@@ -16,7 +17,7 @@ class Carousel extends React.Component{
                     id: 0,
                     title: "spot-a-potty",
                     subTitle: "A bathroom hosting application with a Ruby on Rails backend and React.js frontend",
-                    imgSrc: spotpotpic,
+                    imgSrc: image1,
                     link: 'https://youtu.be/JngmKP10AZs',
                     selected: false
                 },
@@ -24,16 +25,26 @@ class Carousel extends React.Component{
                     id:1,
                     title: "Christy Tropila",
                     subTitle: "Blog site",
-                    imgSrc: profilepic,
                     link: 'https://tropila04.medium.com',
+                    imgSrc: profilepic,
+                    subTitle2: "Linkedin",
+                    link2: 'https://www.linkedin.com/in/christytropila/',
                     selected: false
                 },
                 {
                     id: 2,
                     title: "Sweaterweather",
                     subTitle: "An autumn pinterest clone made with a Ruby on Rails backend and vanilla JS frontend",
-                    imgSrc: sweaterweatherpic,
+                    imgSrc: image3,
                     link: 'https://youtu.be/7n6ZczMmwVo',
+                    selected: false
+                },
+                {
+                    id: 3,
+                    title: "Trick or Treat",
+                    subTitle: "A NYC themed triva game made with a Ruby on Rails backend and React Frontend",
+                    imgSrc: image8,
+                    link: 'https://youtu.be/pzvWNdjSlNU',
                     selected: false
                 },
 
@@ -58,15 +69,15 @@ class Carousel extends React.Component{
 
 makeItems = (items) => {
     return items.map(item => {
-        return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        return <Card border="dark" className="p-8" item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
     })
 }
 
 
 render(){
     return(
-        <Container fluid={true}>
-                <Row className="justify-content-around">
+        <Container fluid={true} >
+                <Row xs={2} md={4} lg={6} className="justify-content-around">
                     {this.makeItems(this.state.items)}
                 </Row>
             </Container>
